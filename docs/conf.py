f8066d6d451ba4ea51cfe1f135cc69696a090e7e
Aaron Bach <bachya1208@gmail.com>
2020-09-30 18:05:57 -0600

    Bump version to 9.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Aaron Bach"
 author = "Aaron Bach"
 
 # The full version, including alpha/beta/rc tags
-release = "9.4.0"
+release = "9.4.1"
 
 
 # -- General configuration ---------------------------------------------------
